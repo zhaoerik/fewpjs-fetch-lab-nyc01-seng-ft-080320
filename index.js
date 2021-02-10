8faf2e4b2efa1b4af75d70283941aad55696d0b9
Erik Zhao <erikzhao98@gmail.com>
2021-02-10 00:54:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function renderBooks(books) {
   const main = document.querySelector('main')
   books.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerText = book.name
     main.appendChild(h2)
   })
 }
